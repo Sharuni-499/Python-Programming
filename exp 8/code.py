e51f8cc35078b23c86152f69ac446e7cda969a26
Sharuni-499 <juug24bca17512@jainuniversity.ac.in>
2026-04-15 23:09:06 +0530

    Create code.py

---
+++ exp 8/code.py
@@ -0,0 +1,50 @@
+import re
+
+date = input("Enter date (DD/MM/YY): ")
+
+text = "brown fox jumps over brown hill"
+key = "id1234"
+
+word = "brown"
+
+# match
+res = re.match("id", key)
+if res:
+    print("Key is valid")
+else:
+    print("Invalid key")
+
+# search
+res = re.search(word, text)
+if res:
+    print("Text valid (case 1)")
+else:
+    print("Text invalid")
+
+# findall
+res = re.findall(word, text)
+if res:
+    print("Text valid (case 2)")
+else:
+    print("Invalid text")
+
+# replace
+new = re.sub("fox", "dog", text)
+print(new)
+
+# extract numbers
+nums = re.findall("[0-9]+", key)
+print(nums)
+
+# date validation
+pattern = r"\d{2}/\d{2}/\d{2}$"
+check = re.match(pattern, date)
+
+if check:
+    print("Valid date")
+else:
+    print("Invalid date")
+
+
+
+exp 8
