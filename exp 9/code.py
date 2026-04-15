25e31c719ec32d6a0991e4a167c4e91efe915fad
Sharuni-499 <juug24bca17512@jainuniversity.ac.in>
2026-04-15 23:20:40 +0530

    Update code.py

--- exp 9/code.py
+++ exp 9/code.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+data = pd.DataFrame({
+    "menu": ["Pizza", "Burger", "Fries", "Coke", "Pasta", "Biryani", "Butter Chicken", "Tiramisu", "Pancakes", "Ice Cream"],
+    "price": [350, 200, 150, 70, 220, 250, 170, 350, 250, 110],
+    "brands": ["Gucci", "LV", "Dior", "Louboutin", "YSL", "Chanel", "Versace", "Hermes", "Armani", "Rolex"],
+    "cars": ["Mercedes", "BMW", "Audi", "Aston Martin", "Ferrari", "Honda", "Toyota", "Porsche", "Bugatti", "KTM"],
+    "games": ["GTA", "Candy Crush", "WWM", "RDR", "RDR2", "Wukong", "Valorant", "COD", "WT", "Genshin"],
+    "animals": ["Cat", "Lion", "Leopard", "Tiger", "Panther", "Cheetah", "Dog", "Elephant", "Wolf", "Fox"],
+    "colors": ["Red", "Blue", "Green", "Black", "White", "Yellow", "Purple", "Orange", "Pink", "Brown"],
+    "cities": ["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune", "Jaipur", "Goa", "Kochi"],
+    "sports": ["Cricket", "Football", "Tennis", "Hockey", "Badminton", "Basketball", "Kabaddi", "F1", "Golf", "Boxing"],
+    "devices": ["Phone", "Laptop", "Tablet", "Watch", "PC", "Camera", "TV", "Speaker", "Console", "Drone"]
+})
+
+print(data)
+
+
+
+exp 9
